7c7e8c4cb1207709a24a72e12bbdca41232f9860
Kalinga Ray <kalinga.ray@externedl.de>
2024-05-07 15:02:21 +0200

    Add a sql file

---
+++ src/employee.sql
@@ -0,0 +1,19 @@
+CREATE TABLE employees
+                 (id INTEGER PRIMARY KEY,
+                 salary INTEGER,
+                 employee_name TEXT,
+                 contact TEXT);
+INSERT INTO employees (salary, employee_name, contact) VALUES
+(8000, 'Kalinga', 'Ilmenau'),
+(1200, 'Raja', 'Adarsha'),
+(11000, 'Laxman', 'Adarsha'),
+(6500, 'Keti', '42Bati'),
+(7500, 'Rina', 'Nimapara'),
+(9000, 'Adhira', 'Konark'),
+(8000, 'Promod', 'Konark'),
+(7000, 'Sonu', 'Konark'),
+(6500, 'Lipi', 'Pahara'),
+(6500, 'Rupali', 'Pahara'),
+(9000, 'Mithun', 'Niali'),
+(5500, 'sonali', 'Pahara');
+
